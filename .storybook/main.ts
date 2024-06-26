f916aebd529f6b3b1da8f4836bb038b5a10f07e1
Idan Levian <eidan47@gmail.com>
2024-06-26 19:37:09 +0300

    Initiate files with sb, navigation and readme

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,23 @@
 /** @type{import("@storybook/react-webpack5").StorybookConfig} */
 module.exports = {
   stories: ['../components/**/*.stories.?(ts|tsx|js|jsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-react-native-web',
+    '@storybook/addon-webpack5-compiler-swc',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
-  docs: {
-    autodocs: true,
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
---
+++ preview-source/README.md
@@ -0,0 +1,6 @@
+I use those previews from the internet. All the credit for the design goes to: `Saepul Nahwan`
+
+Sources:
+* https://dribbble.com/shots/6563578-Pokedex-App-Animation
+* https://dribbble.com/shots/6545819-Pokedex-App
+* https://dribbble.com/shots/6540871-Pokedex-App
