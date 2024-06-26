c595cc87a704baecb64aaacdd46655f736c393bf
Idan Levian <eidan47@gmail.com>
2024-06-26 14:59:48 +0300

    Initial commit
    Generated by react-native@0.73.8

---
+++ components/Button/Button.stories.tsx
@@ -0,0 +1,20 @@
+// stories/MyButton.stories.tsx
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { MyButton } from "./Button";
+
+const meta = {
+  title: "components/MyButton",
+  component: MyButton,
+} satisfies Meta<typeof MyButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {
+    text: "Hello World",
+    color: "purple",
+  },
+};
---
+++ metro.config.js
@@ -0,0 +1,42 @@
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const path = require("path");
+
+const { generate } = require("@storybook/react-native/scripts/generate");
+
+generate({
+  configPath: path.resolve(__dirname, "./.ondevice"),
+});
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  transformer: {
+    unstable_allowRequireContext: true,
+  },
+  resolver: {
+    resolveRequest: (context, moduleName, platform) => {
+      const defaultResolveResult = context.resolveRequest(
+        context,
+        moduleName,
+        platform
+      );
+
+      if (
+        process.env.STORYBOOK_ENABLED !== "true" &&
+        defaultResolveResult?.filePath?.includes?.(".ondevice/")
+      ) {
+        return {
+          type: "empty",
+        };
+      }
+
+      return defaultResolveResult;
+    },
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
