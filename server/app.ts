5cea1d2b5c742c9edcb9400f1bdd45eecdbd10e0
almogko <almogkko1996@gmail.com>
2024-07-31 21:30:43 +0300

    [PKD-25]: response mock data
    * response mock data
    
    * unused
    
    * add route pokemons
    
    * change to 404 instead of ok

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,17 @@
 import * as http from 'http';
+import MOCK_DATA from './data/mock-data.js'
 
 const server = http.createServer((req, res) => {
-    res.end('Ok');
+    const url = req.url;
+
+    if (url === '/pokemons') {
+        res.setHeader('Content-Type', 'application/json')
+        const MOCK_DATA_JSON = JSON.stringify(MOCK_DATA);
+        return res.end(MOCK_DATA_JSON);
+    }
+
+    res.statusCode = 404;
+    res.end();
 });
 
 server.listen(3000, () => {
