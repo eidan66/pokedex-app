92310e2f21156ac33fe02877ea4ddf2323734594
almogko <almogkko1996@gmail.com>
2024-07-31 14:54:36 +0300

    [PKD-22]: make nodejs server a package
    * add package json
    
    * add start script
    
    * add nodemon
    
    * log startup
    
    * configure package manager to be yarn 3.6.4
    
    * add yarn 3.6.4 and update gitignore

--- server/app.js
+++ server/app.js
@@ -4,4 +4,6 @@ const server = http.createServer((req, res) => {
     res.end('Ok');
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
