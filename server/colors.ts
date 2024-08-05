e0bc140907dbcdfe9e1903663fb3a9a56d4437fb
almogko <almogkko1996@gmail.com>
2024-08-05 19:23:27 +0300

    response with real data

---
+++ server/colors.ts
@@ -0,0 +1,35 @@
+export default {
+    black: '#000000',
+    white: '#ffffff',
+  
+    // Pokemon types colors
+    bug: '#A6B91A',
+    dark: '#705746',
+    dragon: '#6F35FC',
+    electric: '#F7D02C',
+    fairy: '#D685AD',
+    fighting: '#C22E28',
+    fire: '#EE8130',
+    flying: '#A98FF3',
+    ghost: '#735797',
+    grass: '#7AC74C',
+    ground: '#E2BF65',
+    ice: '#96D9D6',
+    normal: '#A8A77A',
+    poison: '#A33EA1',
+    psychic: '#F95587',
+    rock: '#B6A136',
+    steel: '#B7B7CE',
+    water: '#6390F0',
+  
+    // Opacity
+    '0.1': '1a',
+    '0.2': '33',
+    '0.3': '4d',
+    '0.4': '66',
+    '0.5': '80',
+    '0.6': '99',
+    '0.7': 'b3',
+    '0.8': 'cc',
+    '0.9': 'e6',
+  } as  { [key: string]: any };
