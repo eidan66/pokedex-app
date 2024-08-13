76ac38bacae89b11028738bd0150c4a7ea22bbc9
almogko <almogkko1996@gmail.com>
2024-08-13 12:10:27 +0300

    remove js extension

--- server/app.ts
+++ server/app.ts
@@ -3,8 +3,8 @@ import * as querystring from 'querystring';
 import * as url from 'url';
 import { Pokemon, PokemonClient } from 'pokenode-ts';
 
-import COLORS from './colors.js';
-import { APIPageResponse, APIPokemon, APIPokemonType } from './responseTypes.js';
+import COLORS from './colors';
+import { APIPageResponse, APIPokemon, APIPokemonType } from './responseTypes';
 
 
 const server = http.createServer(async (req, res) => {
