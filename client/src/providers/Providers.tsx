68f908161a12a736e00a85d6910fb8d69aa787ab
Idan Levian <41434778+eidan66@users.noreply.github.com>
2024-08-26 12:07:04 +0300

    [Fix]: remove screen margin

--- client/src/providers/Providers.tsx
+++ client/src/providers/Providers.tsx
@@ -1,6 +1,6 @@
 import { useReduxDevToolsExtension } from '@react-navigation/devtools';
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
-import { FunctionComponent, PropsWithChildren } from 'react';
+import React, { FunctionComponent, PropsWithChildren } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as ReduxProvider } from 'react-redux';
@@ -25,7 +25,6 @@ export const Providers: FunctionComponent<PropsWithChildren> = ({ children }) =>
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 10,
     backgroundColor: COLORS.white,
   },
 });
