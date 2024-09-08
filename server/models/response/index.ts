21e14107fc72c399505935d3fc12e719afa51e65
almogko <almogkko1996@gmail.com>
2024-09-09 00:34:56 +0300

    [PKD-47]: migrate to python
    migrate to python from node

--- server/models/response/index.ts
+++
@@ -1,7 +0,0 @@
-export interface APIPageResponse<T> {
-    count: number,
-    next?:string | null,
-    previous?: string | null, 
-    results: T[]
-}
-
---
+++ server/pokedexServer/models/pokemon/Colors.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class Colors(Enum):
+    # Pokemon types colors
+    Bug = '#A6B91A',
+    Dark = '#705746',
+    Dragon = '#6F35FC',
+    Electric = '#F7D02C',
+    Fairy = '#D685AD',
+    Fighting = '#C22E28',
+    Fire = '#EE8130',
+    Flying = '#A98FF3',
+    Ghost = '#735797',
+    Grass = '#7AC74C',
+    Ground = '#E2BF65',
+    Ice = '#96D9D6',
+    Normal = '#A8A77A',
+    Poison = '#A33EA1',
+    Psychic = '#F95587',
+    Rock = '#B6A136',
+    Steel = '#B7B7CE',
+    Water ='#6390F0'
