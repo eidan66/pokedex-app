fea8a527540dda2db6fda1f35f6d5e2af1e0e91e
Idan Levian <41434778+eidan66@users.noreply.github.com>
2024-09-11 15:36:23 +0300

    Update folder structure and add dockerfile

--- server/pokedexServer/serializers/pokemon_info_serializers.py
+++ server/pokedexServer/serializers/pokemon_info_serializers.py
@@ -1,26 +1,4 @@
-# serializers.py
-
 from rest_framework import serializers
-
-
-class PokemonSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=100)
-    number = serializers.CharField(max_length=50)
-    types = serializers.ListField(child=serializers.CharField(max_length=50))
-    boxBg = serializers.CharField(max_length=100)
-    svg = serializers.URLField()
-    error = serializers.CharField(max_length=200, required=False)
-
-
-class PokemonDetailSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=100)
-    padded_id = serializers.CharField(max_length=10)
-    types = serializers.ListField(child=serializers.CharField(max_length=50))
-    color = serializers.CharField(max_length=50)
-    sprite_url = serializers.URLField()
-
 
 class AbilitySerializer(serializers.Serializer):
     name = serializers.CharField()
